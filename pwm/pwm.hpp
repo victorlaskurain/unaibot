570f30aa3d6bc76dfe27f1d579e3273f53cbc459
Victor Laskurain <victorlaskurain@gmail.com>
2017-01-06 11:49:26 +0100

    Fix comment.

--- pwm/pwm.hpp
+++ pwm/pwm.hpp
@@ -16,7 +16,7 @@ namespace vla {
 
     /*
      * Timer output	Arduino output	Chip pin	Pin name
-     * OC0A	                     6        12        PD6
+     * OC0A                      6        12        PD6
      * OC0B                      5        11        PD5
      * OC1A                      9        15        PB1
      * OC1B                     10        16        PB2
