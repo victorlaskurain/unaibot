0fd2f818c4fc8f24c9d5237701cda1a3e8925b20
Victor Laskurain <victorlaskurain@gmail.com>
2017-03-14 23:55:12 +0100

    Source ffmpeg configuration if needed.

--- web_control/install.sh
+++ web_control/install.sh
@@ -10,7 +10,8 @@ cp -rL unaibot_web.js html lib node_modules $INSTALLDIR/share
 cp -rL etc/* $INSTALLDIR/etc/
 cat > $INSTALLDIR/bin/unaibot_web <<EOF
 #!/bin/bash
-test -f /opt/node.login && . /opt/node.login
+test -f /opt/node.login   && . /opt/node.login
+test -f /opt/ffmpeg.login && . /opt/ffmpeg.login
 node /opt/unaibot/share/unaibot_web.js --serial-port="\$SERIAL_PORT" --http-basedir="\$HTTP_BASEDIR" --http-port="\$HTTP_PORT" 2>&1 | logger -t unaibot_web
 EOF
 chmod a+x $INSTALLDIR/bin/unaibot_web
