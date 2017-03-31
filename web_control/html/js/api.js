d9559d48a62e560d8dd7eea78559ca109c35ec30
Victor Laskurain <victorlaskurain@gmail.com>
2017-04-01 00:21:51 +0200

    Do not indent first level of module content.
    No need to indent module contents just because they are in a closure.

--- web_control/html/js/api.js
+++ web_control/html/js/api.js
@@ -1,164 +1,166 @@
 define(['jquery'],
 function($) {
-    console.log('BEGIN api');
-    const MOTOR_IDLE     = 0x00,
-          MOTOR_FORWARD  = 0x01,
-          MOTOR_BACKWARD = 0x02,
-          MOTOR_STOP     = 0x11,
-          MOTOR_FIRST    = MOTOR_IDLE,
-          MOTOR_LAST     = MOTOR_STOP;
-    const MESSAGE_PING   = 'PING!',
-          MESSAGE_SPEED2 = 'SPEED2',
-          MESSAGE_CAMON  = 'CAMON',
-          MESSAGE_CAMOFF = 'CAMOFF';
-    var server     = null,
-        msgCounter = 0,
-        promises   = {},
-        m0Speed    = 0.0,
-        m1Speed    = 0.0;
 
-    function _discretize(f) {
-        if (f < 0) {
-            return -_discretizePositive(-f);
+console.log('BEGIN api');
+const MOTOR_IDLE     = 0x00,
+      MOTOR_FORWARD  = 0x01,
+      MOTOR_BACKWARD = 0x02,
+      MOTOR_STOP     = 0x11,
+      MOTOR_FIRST    = MOTOR_IDLE,
+      MOTOR_LAST     = MOTOR_STOP;
+const MESSAGE_PING   = 'PING!',
+      MESSAGE_SPEED2 = 'SPEED2',
+      MESSAGE_CAMON  = 'CAMON',
+      MESSAGE_CAMOFF = 'CAMOFF';
+var server     = null,
+    msgCounter = 0,
+    promises   = {},
+    m0Speed    = 0.0,
+    m1Speed    = 0.0;
+
+function _discretize(f) {
+    if (f < 0) {
+        return -_discretizePositive(-f);
+    } else {
+        return  _discretizePositive(f);
+    }
+}
+
+function _discretizePositive(f) {
+    var steps = [
+        [0.1, 0],
+        [0.4, 127], //  50%
+        [0.7, 189], //  75%
+        [1.0, 255]  // 100%
+    ], i;
+    for (i = 0; i < steps.length; ++i) {
+        if (f < steps[i][0]) {
+            return steps[i][1];
+        }
+    }
+    return 0;
+}
+
+function _makeMessagePromise(msgType, msgData) {
+    var deferred = $.Deferred(),
+        msg     = JSON.stringify({
+            id:   msgCounter,
+            type: msgType,
+            data: msgData || null
+        });
+    console.log(['readyState', server.readyState]);
+    if (server.readyState == WebSocket.OPEN) {
+        promises[msgCounter++] = deferred;
+        server.send(msg);
+    } else {
+        deferred.reject();
+    }
+    return deferred.promise();
+}
+
+/**
+ * speed0 and speed1 must be floats [-1, 1].
+ * +1 => full forward
+ * -1 => full backward
+ *  0 => stop
+ */
+function setSpeed2(speed0, speed1) {
+    var d0 = 0, s0 = 0, d1 = 0, s1 = 0;
+    speed0 = _discretize(speed0);
+    speed1 = _discretize(speed1);
+    if (speed0 == m0Speed && speed1 == m1Speed) {
+        return $.when('OK');
+    }
+    if (speed0 > 0) {
+        d0 = MOTOR_FORWARD;
+        s0 = speed0;
+    } else if (speed0 < 0) {
+        d0 = MOTOR_BACKWARD;
+        s0 = -speed0;
+    }
+    if (speed1 > 0) {
+        d1 = MOTOR_FORWARD;
+        s1 = speed1;
+    } else if (speed1 < 0) {
+        d1 = MOTOR_BACKWARD;
+        s1 = -speed1;
+    }
+    m0Speed = speed0;
+    m1Speed = speed1;
+    return setSpeed4(d0, s0, d1, s1);
+}
+
+function setSpeed4(dir0, speed0, dir1, speed1) {
+    var msg     = [dir0, speed0, dir1, speed1],
+        promise = $.Deferred();
+    _makeMessagePromise(MESSAGE_SPEED2, msg).then(function(msg) {
+        if (msg === 'OK') {
+            promise.resolve(msg);
         } else {
-            return  _discretizePositive(f);
+            promise.reject(msg);
         }
-    }
+    }, promise.reject.bind(promise));
+    return promise;
+}
 
-    function _discretizePositive(f) {
-        var steps = [
-            [0.1, 0],
-            [0.4, 127], //  50%
-            [0.7, 189], //  75%
-            [1.0, 255]  // 100%
-        ], i;
-        for (i = 0; i < steps.length; ++i) {
-            if (f < steps[i][0]) {
-                return steps[i][1];
-            }
-        }
-        return 0;
-    }
+function ping() {
+    return _makeMessagePromise(MESSAGE_PING);
+}
 
-    function _makeMessagePromise(msgType, msgData) {
-        var deferred = $.Deferred(),
-            msg     = JSON.stringify({
-                id:   msgCounter,
-                type: msgType,
-                data: msgData || null
-            });
-        console.log(['readyState', server.readyState]);
-        if (server.readyState == WebSocket.OPEN) {
-            promises[msgCounter++] = deferred;
-            server.send(msg);
-        } else {
-            deferred.reject();
-        }
-        return deferred.promise();
-    }
+function camOn() {
+    return _makeMessagePromise(MESSAGE_CAMON);
+}
 
-    /**
-     * speed0 and speed1 must be floats [-1, 1].
-     * +1 => full forward
-     * -1 => full backward
-     *  0 => stop
-     */
-    function setSpeed2(speed0, speed1) {
-        var d0 = 0, s0 = 0, d1 = 0, s1 = 0;
-        speed0 = _discretize(speed0);
-        speed1 = _discretize(speed1);
-        if (speed0 == m0Speed && speed1 == m1Speed) {
-            return $.when('OK');
-        }
-        if (speed0 > 0) {
-            d0 = MOTOR_FORWARD;
-            s0 = speed0;
-        } else if (speed0 < 0) {
-            d0 = MOTOR_BACKWARD;
-            s0 = -speed0;
-        }
-        if (speed1 > 0) {
-            d1 = MOTOR_FORWARD;
-            s1 = speed1;
-        } else if (speed1 < 0) {
-            d1 = MOTOR_BACKWARD;
-            s1 = -speed1;
-        }
-        m0Speed = speed0;
-        m1Speed = speed1;
-        return setSpeed4(d0, s0, d1, s1);
-    }
+function camOff() {
+    return _makeMessagePromise(MESSAGE_CAMOFF);
+}
 
-    function setSpeed4(dir0, speed0, dir1, speed1) {
-        var msg     = [dir0, speed0, dir1, speed1],
-            promise = $.Deferred();
-        _makeMessagePromise(MESSAGE_SPEED2, msg).then(function(msg) {
-            if (msg === 'OK') {
-                promise.resolve(msg);
-            } else {
-                promise.reject(msg);
-            }
-        }, promise.reject.bind(promise));
-        return promise;
-    }
-
-    function ping() {
-        return _makeMessagePromise(MESSAGE_PING);
-    }
-
-    function camOn() {
-        return _makeMessagePromise(MESSAGE_CAMON);
-    }
-
-    function camOff() {
-        return _makeMessagePromise(MESSAGE_CAMOFF);
-    }
-
-    function _init() {
-        server = new window.WebSocket('ws://' + window.location.host + '/unaibot/cmd');
-        server.onerror = function(evt) {
-            console.log(['WS ERROR', evt]);
-        };
-        server.onopen = function(evt) {
-            console.log(['WS OPENED', evt]);
-        };
-        // reject all pending messages and try to reconnect
-        server.onclose = function(evt) {
-            var promiseId;
-            for (promiseId in promises) {
-                promises[promiseId].reject();
-                delete promises[promiseId];
-            }
-            _init();
-        };
-        server.onmessage = function(evt) {
-            var msg     = JSON.parse(evt.data),
-                promise = promises[msg.id];
-            if (!promise && !msg.evt) {
-                console.log(['UNEXPECTED DATA', msg]);
-                return;
-            }
-            if (promise) {
-                delete promises[msg.id];
-                promise.resolve(msg.data);
-            }
-            if (msg.evt) {
-                $(document).trigger(msg.evt);
-            }
-        };
-    }
-
-    _init();
-
-    console.log('END api');
-    return {
-        ping: ping,
-        setSpeed2: setSpeed2,
-        camOff: camOff,
-        camOn: camOn,
-        MOTOR_IDLE: MOTOR_IDLE,
-        MOTOR_FORWARD: MOTOR_FORWARD,
-        MOTOR_BACKWARD: MOTOR_BACKWARD,
-        MOTOR_STOP: MOTOR_STOP
+function _init() {
+    server = new window.WebSocket('ws://' + window.location.host + '/unaibot/cmd');
+    server.onerror = function(evt) {
+        console.log(['WS ERROR', evt]);
     };
+    server.onopen = function(evt) {
+        console.log(['WS OPENED', evt]);
+    };
+    // reject all pending messages and try to reconnect
+    server.onclose = function(evt) {
+        var promiseId;
+        for (promiseId in promises) {
+            promises[promiseId].reject();
+            delete promises[promiseId];
+        }
+        _init();
+    };
+    server.onmessage = function(evt) {
+        var msg     = JSON.parse(evt.data),
+            promise = promises[msg.id];
+        if (!promise && !msg.evt) {
+            console.log(['UNEXPECTED DATA', msg]);
+            return;
+        }
+        if (promise) {
+            delete promises[msg.id];
+            promise.resolve(msg.data);
+        }
+        if (msg.evt) {
+            $(document).trigger(msg.evt);
+        }
+    };
+}
+
+_init();
+
+console.log('END api');
+return {
+    ping: ping,
+    setSpeed2: setSpeed2,
+    camOff: camOff,
+    camOn: camOn,
+    MOTOR_IDLE: MOTOR_IDLE,
+    MOTOR_FORWARD: MOTOR_FORWARD,
+    MOTOR_BACKWARD: MOTOR_BACKWARD,
+    MOTOR_STOP: MOTOR_STOP
+};
+
 });
--- web_control/html/js/main.js
+++ web_control/html/js/main.js
@@ -1,86 +1,87 @@
 define(['api'],
 function(api) {
-    var doc               = window.document,
-        docEl             = doc.documentElement,
-        requestFullScreen = docEl.requestFullscreen ||
-                            docEl.mozRequestFullScreen ||
-                            docEl.webkitRequestFullScreen ||
-                            docEl.msRequestFullscreen,
-        cancelFullScreen  = doc.exitFullscreen ||
-                            doc.mozCancelFullScreen ||
-                            doc.webkitExitFullscreen ||
-                            doc.msExitFullscreen,
-        left              = document.getElementById('left'),
-        right             = document.getElementById('right'),
-        lSpeed            = null,
-        rSpeed            = null,
-        camToggle         = document.getElementById('cam-toggle');
 
-    window.document.documentElement.addEventListener('touchstart', function(evt) {
-        switch (evt.target) {
-        case left:
-            lSpeed = 0;
-            break;
-        case right:
-            rSpeed = 0;
-            break;
-        default:
-            // do nothing
+var doc               = window.document,
+    docEl             = doc.documentElement,
+    requestFullScreen = docEl.requestFullscreen ||
+                        docEl.mozRequestFullScreen ||
+                        docEl.webkitRequestFullScreen ||
+                        docEl.msRequestFullscreen,
+    cancelFullScreen  = doc.exitFullscreen ||
+                        doc.mozCancelFullScreen ||
+                        doc.webkitExitFullscreen ||
+                        doc.msExitFullscreen,
+    left              = document.getElementById('left'),
+    right             = document.getElementById('right'),
+    lSpeed            = null,
+    rSpeed            = null,
+    camToggle         = document.getElementById('cam-toggle');
+
+window.document.documentElement.addEventListener('touchstart', function(evt) {
+    switch (evt.target) {
+    case left:
+        lSpeed = 0;
+        break;
+    case right:
+        rSpeed = 0;
+        break;
+    default:
+        // do nothing
+    }
+});
+
+window.document.documentElement.addEventListener('touchend', function(evt) {
+    switch (evt.target) {
+    case left:
+        lSpeed = null;
+        break;
+    case right:
+        rSpeed = null;
+        break;
+    default:
+        // do nothing
+    }
+    api.setSpeed2(0, 0);
+});
+
+window.document.documentElement.addEventListener('touchmove', function(evt) {
+    var i, height = left.clientHeight, speed;
+    if (lSpeed !== null && rSpeed !== null) {
+        for (i = 0; i < evt.touches.length; ++i) {
+            speed = 1 - evt.touches[i].pageY / (height / 2);
+            switch (evt.touches[i].target) {
+            case left:
+                lSpeed = speed;
+                break;
+            case right:
+                rSpeed = speed;
+                break;
+            default:
+                // do nothing
+            }
         }
-    });
-
-    window.document.documentElement.addEventListener('touchend', function(evt) {
-        switch (evt.target) {
-        case left:
-            lSpeed = null;
-            break;
-        case right:
-            rSpeed = null;
-            break;
-        default:
-            // do nothing
-        }
-        api.setSpeed2(0, 0);
-    });
-
-    window.document.documentElement.addEventListener('touchmove', function(evt) {
-        var i, height = left.clientHeight, speed;
         if (lSpeed !== null && rSpeed !== null) {
-            for (i = 0; i < evt.touches.length; ++i) {
-                speed = 1 - evt.touches[i].pageY / (height / 2);
-                switch (evt.touches[i].target) {
-                case left:
-                    lSpeed = speed;
-                    break;
-                case right:
-                    rSpeed = speed;
-                    break;
-                default:
-                    // do nothing
-                }
-            }
-            if (lSpeed !== null && rSpeed !== null) {
-                api.setSpeed2(lSpeed, rSpeed);
-            }
+            api.setSpeed2(lSpeed, rSpeed);
         }
-    });
+    }
+});
 
-    $(document).on('videoOff', function() {
-        camToggle.className = 'off';
-        return true;
-    });
+$(document).on('videoOff', function() {
+    camToggle.className = 'off';
+    return true;
+});
 
-    $(document).on('videoOn', function() {
-        camToggle.className = 'on';
-        return true;
-    });
+$(document).on('videoOn', function() {
+    camToggle.className = 'on';
+    return true;
+});
 
-    camToggle.addEventListener('click', function(evt) {
-        if (camToggle.classList.contains('off')) {
-            api.camOn();
-        } else {
-            api.camOff();
-        }
-    });
+camToggle.addEventListener('click', function(evt) {
+    if (camToggle.classList.contains('off')) {
+        api.camOn();
+    } else {
+        api.camOff();
+    }
+});
 
 });
